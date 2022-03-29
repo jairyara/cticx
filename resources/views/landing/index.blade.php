28c2982da6dafc7c36ae0b4863dd035382adbc19
Jair Yara <jyara@oneago.com>
2022-03-28 21:19:02 -0500

    Set first files for project

---
+++ resources/views/landing/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.master')
+
+@section('pageTitle', 'Inicio')
+
+@section('content')
+    <h1>Hola</h1>
+@stop
---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('pageTitle') - CTICX</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+@yield('content')
+</body>
+</html>
