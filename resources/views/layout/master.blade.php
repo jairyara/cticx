29e2272ac726efb4b8d0b83743ed18c967a6f919
Jair Yara <jair.yara11@gmail.com>
2022-05-29 15:37:15 -0500

    Add styles

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -11,11 +11,15 @@
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
-
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script type="module" src="{{ asset('js/index.js') }}"></script>
 </head>
 <body>
 @yield('content')
-
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+<script>
+    AOS.init();
+</script>
 </body>
 </html>
