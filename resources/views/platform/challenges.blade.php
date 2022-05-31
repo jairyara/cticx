5973b466f7888a2804095f8d02d0a4ea4fb54c88
Jair Yara <jair.yara11@gmail.com>
2022-05-30 22:52:11 -0500

    Add styles to form

--- resources/views/platform/challenges.blade.php
+++ resources/views/platform/challenges.blade.php
@@ -6,7 +6,7 @@
 
     <section class="challenges">
         <h3>Crear reto</h3>
-        <form>
+        <form method="POST">
             <div class="label-form">
                 <label for="question">Título</label>
                 <input id="question" name="question" type="text" placeholder="Ingrese la pregunta" required>
@@ -15,18 +15,13 @@
                 <label for="category">Categoría</label>
                 <select name="category" id="category">
                     <option selected disabled>Seleccione una opción</option>
-                    <option value=""></option>
+                    <option value="0">Ciberseguridad</option>
+                    <option value="1">Ingeniería social</option>
                 </select>
             </div>
             <div class="label-form">
                 <label for="answer">Solución</label>
-                <select name="answer" id="answer">
-                    <option selected disabled>Seleccione una opción</option>
-                    <option value=""></option>
-                    <option value=""></option>
-                    <option value=""></option>
-                    <option value=""></option>
-                </select>
+                <input name="answer" id="answer" type="text" required>
             </div>
             <button class="btn btn-primary" type="submit">Guardar</button>
         </form>
